1939ebe23fff9a34862e2714ad5abd6113a4030a
bpotter-trinity <bpotter@trinity.edu>
2021-06-29 19:39:18 -0500

    added ref btn, need particles, scroll and fade

--- nytelock-section/src/sections/Section.js
+++ nytelock-section/src/sections/Section.js
@@ -2,15 +2,17 @@ import React from 'react'
 import './section.css';
 import Selection from '../components/Selection.js';
 //import Particles from 'react-particles-js'
+import contactArrow from '../imgs/contactArrow.png';
 
 export default function Section() {
     return (
         <div className="section">
-            <p> </p>
             
             <Selection/>
             
-            
+            <div className="refBtn">
+                <button onClick={() => window.location.assign('https://nytelock.com/logo-design-service-in-singapore/')}className='contactBtn'>Section Reference {<img className='arrow' src={contactArrow} alt='none'></img>}</button>
+            </div>
         </div>
     )
 }
